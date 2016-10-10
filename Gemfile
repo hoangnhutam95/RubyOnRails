5bbff47cbe4dd3cec4029d84ebf63f49dd31ddc1
hoangnhutam95 <hoangnhutam95@gmail.com>
2016-10-11 04:29:42 +0700

    update

--- Gemfile
+++ Gemfile
@@ -43,7 +43,8 @@ group :development do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
-  gem "bcrypt-ruby"
+  gem "bcrypt-ruby" 
+  gem 'unicorn'
 
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root 'static_pages#home'
   get 'static_pages/home'
 
   get 'static_pages/help'
+  get 'signup'=>'user#new'
+  get 'login'=> 'sessions#new'
+  post 'login'=> 'sessions#create'
+  delete 'logout'=>'sessions#destroy'
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
